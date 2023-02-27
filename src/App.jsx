cdcb54a873115e2b48a079860935fff9aae6fb77
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2023-02-27 07:25:33 +0200

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,9 @@ function App() {
     <main>
       <div className='flex flex-col justify-center items-center w-full
                       shadow-lg rounded-3xl md:max-w-3xl md:flex-row'>
-        <div className='flex flex-col w-full justify-center items-center
+        <div className='flex flex-col w-full max-w-md justify-center items-center
                         bg-gradient-to-t from-grLightRoyalBluebg to-grLightSlateBluebg
-                        px-10 py-8 text-center text-lightLavender rounded-b-3xl
+                        px-10 py-10 text-center text-lightLavender rounded-b-3xl
                         md:w-1/2 md:rounded-3xl'>
 
           <h3 className='text-lg mb-4'>Your Results</h3>
@@ -27,8 +27,8 @@ function App() {
             <h1 className='text-white text-5xl font-extrabold'>76</h1>
             <span className='text-sm'>of 100</span>
           </div>
-          <h4 className='text-white mb-4 text-lg font-semibold'>Great</h4>
-          <p className='text-center p-4'>You scored higher than 65% of the people who have taken these results</p>
+          <h4 className='text-white mb-4 text-2xl font-semibold'>Great</h4>
+          <p className='text-center px-4'>You scored higher than 65% of the people who have taken these results</p>
         </div>
         <div className='w-full px-6 my-2 md:w-1/2'>
           <h3 className='my-4 text-2xl font-bold text-darkGrayBlue'>Summary</h3>
