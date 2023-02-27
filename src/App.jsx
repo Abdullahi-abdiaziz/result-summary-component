6daee984e9d67e9c4970c4ced95e1d01852ae025
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2023-02-27 07:37:11 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,21 +16,21 @@ function App() {
   return (
     <main>
       <div className='flex flex-col justify-center items-center w-full
-                      shadow-lg rounded-3xl md:max-w-3xl md:flex-row'>
+                      shadow-lg rounded-3xl md:max-w-4xl md:flex-row'>
         <div className='flex flex-col w-full max-w-md justify-center items-center
                         bg-gradient-to-t from-grLightRoyalBluebg to-grLightSlateBluebg
-                        px-10 py-10 text-center text-lightLavender rounded-b-3xl
+                        px-10 py-12 text-center text-lightLavender rounded-b-3xl
                         md:w-1/2 md:rounded-3xl'>
 
-          <h3 className='text-lg mb-4'>Your Results</h3>
-          <div className='py-9 px-12 mb-6 rounded-full bg-gradient-to-t from-grVioletBlue to-grPersianbBlue'>
+          <h4 className='text-xl mb-6'>Your Results</h4>
+          <div className='py-9 px-12 mb-10 rounded-full bg-gradient-to-t from-grVioletBlue to-grPersianbBlue'>
             <h1 className='text-white text-5xl font-extrabold'>76</h1>
             <span className='text-sm'>of 100</span>
           </div>
-          <h4 className='text-white mb-4 text-2xl font-semibold'>Great</h4>
-          <p className='text-center px-4'>You scored higher than 65% of the people who have taken these results</p>
+          <h5 className='text-white mb-4 text-2xl font-semibold'>Great</h5>
+          <p className='text-center text-lg px-4'>You scored higher than 65% of the people who have taken these results</p>
         </div>
-        <div className='w-full px-6 my-2 md:w-1/2'>
+        <div className='w-full px-10 py-10 my-2 md:w-1/2'>
           <h3 className='my-4 text-2xl font-bold text-darkGrayBlue'>Summary</h3>
           {items}
           <a href="#" className="mx-2 my-4 block text-center text-white font-semibold py-3 rounded-full bg-darkGrayBlue">Continue</a>
